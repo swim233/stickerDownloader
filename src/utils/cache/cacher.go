83cf8da8b43b3381175ff601c529104346ebddc9
awwwsl <awwwsl.wdnmd@gmail.com>
2025-04-19 16:24:40 +0800

    fix: cache time not applied & high cpu consumption

--- src/utils/cache/cacher.go
+++ src/utils/cache/cacher.go
@@ -7,7 +7,7 @@ import (
 	"github.com/swim233/StickerDownloader/utils"
 )
 
-var Cacher = cache.New(time.Duration(utils.BotConfig.CacheExpirationTime*60), time.Duration(600))
+var Cacher = cache.New(time.Duration(utils.BotConfig.CacheExpirationTime)*time.Minute, 10*time.Minute)
 
 // 添加缓存
 func AddCache(name string, inputData []byte) {
