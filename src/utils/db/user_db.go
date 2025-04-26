867ef76bc1b39f87976f798416144166052037c1
swim233 <swim853279614@163.com>
2025-04-26 15:16:53 +0800

    feat : 增加数据库功能

---
+++ src/utils/db/user_db.go
@@ -0,0 +1,114 @@
+package db
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"os"
+	"time"
+
+	tgbotapi "github.com/ijnkawakaze/telegram-bot-api"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+type UserData struct {
+	UserID             int64  `gorm:"primaryKey,autoIncrement:false"`
+	FirstName          string `gorm:"not null"`
+	LastName           string
+	UserName           string
+	DownloadFiles      int
+	DownloadFileSize   int64
+	CreateTime         string `gorm:"not null,default:0"`
+	RecentDownloadTime string `gorm:"not null,default:0"`
+}
+
+type StickerData struct {
+	StickerName        string `gorm:"primaryKey,autoIncrement:false,not null"`
+	StickerTitle       string `gorm:"not null"`
+	RecentDownloadTime string `gorm:"not null,default:0"`
+	DownloadCount      int
+}
+
+var DB *gorm.DB
+
+func InitDB() {
+	os.MkdirAll("db", 0700)
+	db, err := gorm.Open(sqlite.Open("db/data.db"), &gorm.Config{})
+	if err != nil {
+		fmt.Printf("数据库初始化失败: %v\n", err)
+		os.Exit(1)
+	}
+	DB = db
+	DB.AutoMigrate(&UserData{}, &StickerData{})
+}
+func RecordUserData(u tgbotapi.Update, fileSize int64, fileCount int) {
+	user := u.CallbackQuery.From
+	if user == nil {
+		return
+	}
+	newUser := UserData{}
+
+	err := DB.Where("user_id = ?", user.ID).First(&newUser).Error
+	if err != nil && errors.Is(err, gorm.ErrRecordNotFound) {
+		err := DB.Create(UserData{
+			UserID:             user.ID,
+			FirstName:          user.FirstName,
+			LastName:           user.LastName,
+			UserName:           user.UserName,
+			DownloadFiles:      fileCount,
+			DownloadFileSize:   fileSize,
+			CreateTime:         time.Now().Format(time.RFC3339),
+			RecentDownloadTime: time.Now().Format(time.RFC3339),
+		}).Error
+		if err != nil {
+			DB.Logger.Error(context.Background(), err.Error())
+		} else {
+			DB.Logger.Info(context.Background(), "创建成功")
+		}
+	} else if err != nil {
+		fmt.Printf("err: %v\n", err)
+	} else {
+		newUser.DownloadFiles += fileCount
+		newUser.DownloadFileSize += fileSize
+		newUser.FirstName = user.FirstName
+		newUser.LastName = user.LastName
+		newUser.UserName = user.UserName
+		newUser.RecentDownloadTime = time.Now().Format(time.RFC3339)
+		err := DB.Where("user_id = ?", user.ID).Save(&newUser).Error
+		if err != nil {
+			DB.Logger.Error(context.Background(), err.Error())
+		}
+	}
+
+}
+
+func RecordStickerData(name string, title string) {
+	newStickerSetData := StickerData{}
+
+	err := DB.Where("sticker_name = ?", name).First(&newStickerSetData).Error
+	if err != nil && errors.Is(err, gorm.ErrRecordNotFound) {
+		err := DB.Create(StickerData{
+			StickerName:        name,
+			StickerTitle:       title,
+			DownloadCount:      1,
+			RecentDownloadTime: time.Now().Format(time.RFC3339),
+		}).Error
+		if err != nil {
+			DB.Logger.Error(context.Background(), err.Error())
+		} else {
+			DB.Logger.Info(context.Background(), "创建成功")
+		}
+	} else if err != nil {
+		fmt.Printf("err: %v\n", err)
+	} else {
+		newStickerSetData.DownloadCount += 1
+		newStickerSetData.StickerTitle = title
+		newStickerSetData.RecentDownloadTime = time.Now().Format(time.RFC3339)
+		err := DB.Where("sticker_name = ?", name).Save(&newStickerSetData).Error
+		if err != nil {
+			DB.Logger.Error(context.Background(), err.Error())
+		}
+	}
+
+}
